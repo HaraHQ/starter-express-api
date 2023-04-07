c64dc623d51afb7b790bc21e01be0f8657195467
Beaver Tribemaster <hara.bekerja@gmail.com>
2023-04-08 06:18:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+
+app.get('/download', function(req, res){
+  const file = `${__dirname}/upload/dramaticpenguin.MOV`;
+  res.download(file); // Set disposition and send it.
+});
+
+app.get('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
